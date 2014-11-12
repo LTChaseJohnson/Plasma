100faf838be95a5068e36a6e06908611552d2cf6
LTChaseJohnson <chasevjohnson@gmail.com>
2014-11-12 10:31:08 -0500

    Updated for user input

--- PlasmaHW4PIC.py
+++ PlasmaHW4PIC.py
@@ -3,26 +3,34 @@ import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-q = 1.602E-19
-m = 9.109E-27
-dt = 1E-7
+# Constants for problem
+q = input ('Enter particle charge: ')
+m = input ('Enter particle mass: ')
+dt = input ('Enter time step size: ')
 N = 100
 t = np.linspace(0,dt*N,N)
 
+# Establishes position and velocity arrays depending on
+# desired time steps and final time
 r = zeros((N,3))
 v = zeros((N,3))
 
+# Establishes initial conditions in first index
 r[0] = [0.0, 0.0, 0.0]
 v[0] = [10**5, 10**5, 10**5]
 
+# Constant B and E Fields
 B = array([0.0, 0.0, 0.1])
 E = array([1000.0,0.0,0.0])
 
+# Calculates the acceleration from Newton's Law
+# Uses step approximation to calculate future position
 for i in range(N-1):
     a   = q/m*E + q/m* cross(v[i],B) 
     v[i+1] = v[i] + a*dt
     r[i+1] = r[i] + v[i+1]*dt
 
+# Plots
 figure(figsize=(8,6))
 plot(t, r[:,0], label="x-direction")
 plot(t, r[:,1], label="y-direction")
@@ -39,7 +47,9 @@ xlabel("Time",fontsize=26)
 ylabel("Velocities",fontsize=26)
 legend()
 
+# Creates 3-D position plot
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.plot(r[:,0], r[:,1], r[:,2])
+
 plt.show()
